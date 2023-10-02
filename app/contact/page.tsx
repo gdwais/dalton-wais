ce7662b429ed032d1eb563a40ec16a1f3a01676a
Dalton Wais <48781001+gdwais@users.noreply.github.com>
2023-10-01 22:07:00 -0600

    remove  errant param

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,10 +31,7 @@ export default function Contact() {
       <Navigation currentPage="contact" />
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-          <Particles
-            className="absolute inset-0 -z-10 animate-fade-in"
-            quantity={500}
-          />
+          <Particles className="absolute inset-0 -z-10 animate-fade-in" />
           {socials.map((s) => (
             <Card>
               <Link
